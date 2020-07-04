b74e156930e0ad3554bf1633fc50ece384698e2d
kunihik0 <tokuko2yuu.nikkorishiyou@gmail.com>
2020-07-05 01:22:15 +0900

    test: add colab.py

---
+++ tools/__init__.py
@@ -0,0 +1,3 @@
+from .tool_from_youtube import midpoint
+from .tool_from_youtube import get_blinking_ratio
+from .tool_from_youtube import get_gaze_ratio
---
+++ tools/tool_from_youtube.py
@@ -0,0 +1,88 @@
+
+from math import hypot
+import dlib
+import numpy as np
+import cv2
+
+
+def midpoint(p1, p2):
+    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+
+
+def get_blinking_ratio(eye_points, facial_landmarks):
+    left_point = (facial_landmarks.part(
+        eye_points[0]).x, facial_landmarks.part(eye_points[0]).y)
+    right_point = (facial_landmarks.part(
+        eye_points[3]).x, facial_landmarks.part(eye_points[3]).y)
+
+    # point
+    center_top = midpoint(facial_landmarks.part(
+        eye_points[1]), facial_landmarks.part(eye_points[2]))
+    center_bottom = midpoint(facial_landmarks.part(
+        eye_points[5]), facial_landmarks.part(eye_points[4]))
+
+    # line
+    # hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
+    # ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 2)
+
+    # length
+    hor_line_length = hypot(
+        (left_point[0]-right_point[0]), (left_point[1]-right_point[1]))
+    ver_line_length = hypot(
+        (center_top[0]-center_bottom[0]), (center_top[1]-center_bottom[1]))
+
+    ratio = hor_line_length/ver_line_length
+
+    return ratio
+
+
+def get_gaze_ratio(eye_points, facial_landmarks):
+    # Gaze detection
+    left_eye_resion = np.array([(facial_landmarks.part(0).x, facial_landmarks.part(0).y),
+                                (facial_landmarks.part(1).x,
+                                 facial_landmarks.part(1).y),
+                                (facial_landmarks.part(2).x,
+                                 facial_landmarks.part(2).y),
+                                (facial_landmarks.part(3).x,
+                                 facial_landmarks.part(3).y),
+                                (facial_landmarks.part(4).x,
+                                 facial_landmarks.part(4).y),
+                                (facial_landmarks.part(5).x, facial_landmarks.part(5).y)],
+                               np.int32)
+    # cv2.polylines(frame, [left_eye_resion], True, (0, 0, 255), 2)
+
+    # カメラのdefault sizeの真っ黒window作成　
+    height, width, _ = frame.shape
+    mask = np.zeros((height, width), np.uint8)
+
+    # 目を囲む
+    cv2.polylines(mask, [left_eye_resion], True, 255, 2)
+    # 目ん玉塗りつぶす
+    cv2.fillPoly(mask, [left_eye_resion], 255)
+    # gray(顔全体)を目の部分だけ抽出
+    left_eye = cv2.bitwise_and(gray, gray, mask=mask)
+
+    # 目を長方形windowで抜き出し
+    min_x = np.min(left_eye_resion[:, 0])
+    max_x = np.max(left_eye_resion[:, 0])
+    min_y = np.min(left_eye_resion[:, 1])
+    max_y = np.max(left_eye_resion[:, 1])
+    gray_eye = left_eye[min_y: max_y, min_x:max_x]
+
+    # 白黒にするための閾値を決めて（今回は70）、全体を白黒にする
+    _, threshold_eye = cv2.threshold(
+        gray_eye, 70, 255, cv2.THRESH_BINARY)
+
+    height, width = threshold_eye.shape
+    left_side_threshold = threshold_eye[0:height, 0:int(width/2)]
+    left_side_white = cv2.countNonZero(left_side_threshold)
+
+    right_side_threshold = threshold_eye[0:height, int(width/2): width]
+    right_side_white = cv2.countNonZero(right_side_threshold)
+
+    try:
+        gaze_ratio = left_side_white/right_side_white
+    except ZeroDivisionError:
+        gaze_ratio = left_side_white/(1e-4)
+
+    return gaze_ratio
