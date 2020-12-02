95ab4a510224379531df346a41a0dc1ea67edda0
kunihik0 <tokuko2yuu.nikkorishiyou@gmail.com>
2020-12-02 11:17:58 +0900

    first commit tools

---
+++ tools/indicator.py
@@ -0,0 +1,41 @@
+from copy import copy                                                                      
+from math import hypot
+import os                
+import sys               
+import time              
+                         
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append("../")
+sys.path.append("../tools/")                                   
+                         
+import cv2               
+import dlib              
+import numpy as np       
+                         
+from tools.detector import Detector
+from tools.discriminator import Discriminator
+
+
+class Indicator(object):
+    def __init__(self,frame,gray,landmarks):
+        self.detector=Detector(frame,gray,landmarks)
+        self.discriminator=Discriminator(frame,landmarks)
+
+    def blinking_indicator(self):
+        blinking_ratio=self.detector.blinking_detector()
+        self.discriminator.blinking_discriminator(blinking_ratio, threshold=6)
+        
+        return blinking_ratio
+
+    def gaze_indicator(self,frame,gray,new_frame,default_side_ratio):
+        gaze_side_ratio, gaze_vertical_ratio=self.detector.gaze_detector()
+        self.discriminator.side_discriminator(new_frame,default_side_ratio,gaze_side_ratio)
+        self.discriminator.vertical_discriminator(new_frame,default_side_ratio,gaze_vertical_ratio)
+        return gaze_side_ratio, gaze_vertical_ratio
+
+
+
+
+
+
+
--- tools/tool_from_youtube.py
+++ tools/tool_from_youtube.py
@@ -36,7 +36,7 @@ def get_blinking_ratio(eye_points, facial_landmarks):
     return ratio
 
 
-def get_gaze_ratio(eye_points, facial_landmarks):
+def get_gaze_ratio(frame, gray, eye_points, facial_landmarks):
     # Gaze detection
     left_eye_resion = np.array([(facial_landmarks.part(0).x, facial_landmarks.part(0).y),
                                 (facial_landmarks.part(1).x,
@@ -60,18 +60,18 @@ def get_gaze_ratio(eye_points, facial_landmarks):
     # 目ん玉塗りつぶす
     cv2.fillPoly(mask, [left_eye_resion], 255)
     # gray(顔全体)を目の部分だけ抽出
-    left_eye = cv2.bitwise_and(gray, gray, mask=mask)
+    eye = cv2.bitwise_and(gray, gray, mask=mask)
 
     # 目を長方形windowで抜き出し
     min_x = np.min(left_eye_resion[:, 0])
     max_x = np.max(left_eye_resion[:, 0])
     min_y = np.min(left_eye_resion[:, 1])
     max_y = np.max(left_eye_resion[:, 1])
-    gray_eye = left_eye[min_y: max_y, min_x:max_x]
+    gray_eye = eye[min_y: max_y, min_x:max_x]
 
     # 白黒にするための閾値を決めて（今回は70）、全体を白黒にする
     _, threshold_eye = cv2.threshold(
-        gray_eye, 70, 255, cv2.THRESH_BINARY)
+        gray_eye, 80, 255, cv2.THRESH_BINARY)
 
     height, width = threshold_eye.shape
     left_side_threshold = threshold_eye[0:height, 0:int(width/2)]
@@ -81,8 +81,20 @@ def get_gaze_ratio(eye_points, facial_landmarks):
     right_side_white = cv2.countNonZero(right_side_threshold)
 
     try:
-        gaze_ratio = left_side_white/right_side_white
+        gaze_side_ratio = left_side_white/right_side_white
     except ZeroDivisionError:
-        gaze_ratio = left_side_white/(1e-4)
+        gaze_side_ratio = left_side_white/(1e-4)
 
-    return gaze_ratio
+    upper_side_threshold = threshold_eye[int(height/2):height, 0:width]
+    upper_side_white = cv2.countNonZero(upper_side_threshold)
+
+    lower_side_threshold = threshold_eye[0:int(height/2), 0:width]
+    lower_side_white = cv2.countNonZero(lower_side_threshold)
+
+    try:
+        gaze_vertical_ratio = upper_side_white/lower_side_white
+    except ZeroDivisionError:
+        gaze_vertical_ratio = upper_side_white/(1e-4)
+
+    return gaze_side_ratio, gaze_vertical_ratio
+
